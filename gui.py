f49d8472c36e72a083a0d7a64f135e37ab3a0a3c
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-10-25 18:51:49 +0300

    declared def cipher()

--- gui.py
+++ gui.py
@@ -110,3 +110,123 @@ def copy_password(account):
     We import the framework then declare a function that copies the emails.
     """
     return Attributes.duplicate_password(account)
+
+
+def cipher():
+    print(
+        "Hi There, I'm Cipher, your Accounts Password manager...\n Please enter one of the following to proceed.\n CA "
+        "--- "
+        "(To Create Account)  \n LA ---  (Login To An Existing Account)  \n")
+    short_code = input("").lower().strip()
+    if short_code == "ca":
+        print("Sign Up")
+        print('~' * 50)
+        username = input("User_name: ")
+        while True:
+            print(" CP - (To create your own password)\n GP - (let cipher generate a random password for you)")
+            password_choice = input().lower().strip()
+            if password_choice == 'cp':
+                password = input("Enter Password\n")
+                break
+            elif password_choice == 'gp':
+                password = engender_password()
+                break
+            else:
+                print("Invalid password, please try again")
+        save_user(create_new_user(username, password))
+        print("~" * 85)
+        print(f"Hello {username}, Your account has been created successfully! Your password is: {password}")
+        print("~" * 85)
+    elif short_code == "la":
+
+        print("Enter your Username and Password to log in:")
+        print("~" * 50)
+        username = input("User name: ")
+        password = input("password: ")
+        login = login_user(username, password)
+        if login_user == login:
+            print(f"Hello {username}.Welcome To Cipher")
+            print('\n')
+    while True:
+        print(
+            "Use The Following Short Codes:\n CC - (Create a new credential) \n DC - (Display Credentials) \n LC - ("
+            "Locate a "
+            "credential) \n GP - (let cipher generate a random password for you) \n Del - (Delete credential) \n Ex - ("
+            "Exit the "
+            "application) \n")
+        short_code = input().lower().strip()
+        if short_code == "cc":
+            print("Create New Credential")
+            print("." * 20)
+            print("Account name ....")
+            account = input().lower()
+            print("Your Account username")
+            username = input()
+            while True:
+                print(
+                    "CP - (To create your own password)\n GP - (Let cipher generate a random password for you)")
+                password_choice = input().lower().strip()
+                if password_choice == 'cp':
+                    password = input("Enter Your Own Password\n")
+                    break
+                elif password_choice == 'gp':
+                    password = engender_password()
+                    break
+                else:
+                    print("Invalid password please try again")
+            save_attributes(create_new_attribute(account, username, password))
+            print('\n')
+            print(
+                f"Account Attributes for: Account - {account} - UserName: {username} - Password:{password} created "
+                f"successfully")
+            print('\n')
+        elif short_code == "dc":
+            if display_accounts_details():
+                print(f"Here's your list of accounts : ")
+
+                print('|*' * 15)
+                print('_' * 30)
+                for account in display_accounts_details():
+                    print(f" Account:{account.account} \n User Name:{username}\n Password:{password}")
+                    print('_' * 30)
+                print('*|' * 15)
+            else:
+                print("Sorry, you have no credentials saved yet..........")
+        elif short_code == "lc":
+            print("Enter the Account Name you want to search for")
+            search_name = input().lower()
+            if locate_attribute(search_name):
+                search_attribute = locate_attribute(search_name)
+                print(f"Account Name : {search_attribute.account}")
+                print('-' * 50)
+                print(f"User Name: {search_attribute.username} Password :{search_attribute.password}")
+                print('-' * 50)
+            else:
+                print("Sorry, the credential declared does not exist")
+                print('\n')
+        elif short_code == "del":
+            print("Enter the account name of the Attributes you want to delete")
+            search_name = input().lower()
+            if locate_attribute(search_name):
+                search_attribute = locate_attribute(search_name)
+                print("_" * 50)
+                search_attribute.terminate_attributes()
+                print('\n')
+                print(f"Your stored credentials for : {search_attribute.account} have been successfully deleted!!!")
+                print('\n')
+            else:
+                print("Sorry, the declared credential for deletion does not exist in your store yet")
+
+        elif short_code == 'gp':
+
+            password = engender_password()
+            print(f" {password} Your cipher has been generated successfully. You can proceed to use it in your account")
+        elif short_code == 'ex':
+            print("Thanks for using Cipher.. See you soon!")
+            break
+        else:
+            print("Incorrect... Your entries should correspond with the Menu above")
+
+
+if __name__ == '__main__':
+    cipher()
