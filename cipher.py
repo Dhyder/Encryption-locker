f3d10e35c689d7d9799b1f3fe9be2dee092c570c
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-10-25 18:20:05 +0300

    created classmethod  and declared def for account display and account termination

--- cipher.py
+++ cipher.py
@@ -21,3 +21,13 @@ class User:
         A method that saves a new user instance into the user list
         """
         User.user_list.append(self)
+
+    @classmethod
+    def display_user(cls):
+        return cls.user_list
+
+    def terminate_user(self):
+        """
+        This method deletes a saved account from the list
+        """
+        User.user_list.remove(self)
