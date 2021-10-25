753f709f5d7dc2d31ca568be94c4eb6f46e93ba1
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-10-25 19:00:59 +0300

    created class TestAttributes : def setUp&test_inst

--- cipher_test.py
+++ cipher_test.py
@@ -29,5 +29,25 @@ class TestClass(unittest.TestCase):
         self.assertEqual(len(User.user_list), 1)
 
 
+class TestAttributes(unittest.TestCase):
+    """
+    Test class that defines test cases for attributes class
+    """
+
+    def setUp(self):
+        """
+        Runs before solitary attribute test methods run
+        """
+        self.contemporary_attribute = Attributes("Amino", "Vickie_Shiraa", "Jessica*@hyde7502!")
+
+    def test_inst(self):
+        """
+        Test class that checks if a new attributes instance has been instantiated veraciously
+        """
+        self.assertEqual(self.contemporary_attribute.account, "Amino")
+        self.assertEqual(self.contemporary_attribute.username, "Vickie_Shiraa")
+        self.assertEqual(self.contemporary_attribute.password, "Jessica*@hyde7502!")
+
+
 if __name__ == '__main__':
     unittest.main()
