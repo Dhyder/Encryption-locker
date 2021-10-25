3b45e2271f9ac6410064cd003dc2bbd06146a7d2
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-10-25 18:58:59 +0300

    created class TestClass : def setUp& test_inst&test_stockpile_user

--- cipher_test.py
+++ cipher_test.py
@@ -1,9 +1,32 @@
 import unittest
+from cipher import User
+from cipher import Attributes
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class TestClass(unittest.TestCase):
+    """
+    Defines test cases for the User class
+    """
+
+    def setUp(self):
+        """
+        Runs before solitary test methods run
+        """
+        self.rookie_user = User("Vickie_Shiraa", "Jessica*@hyde7502!")
+
+    def test_inst(self):
+        """
+        Test case to check if the object has been instantiated correctly
+        """
+        self.assertEqual(self.rookie_user.username, "Vickie_Shiraa")
+        self.assertEqual(self.rookie_user.password, "Jessica*@hyde7502!")
+
+    def test_stockpile_user(self):
+        """
+        test case to test if a user instance was saved into the User list
+        """
+        self.rookie_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
 
 
 if __name__ == '__main__':
